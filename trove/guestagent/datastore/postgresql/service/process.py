fe8028b7779d480d6f8989c1388535b81e62785a
Nirav Shah <nshah@tesora.com>
2015-01-22 21:31:24 -0800

    Fix PostgreSQL volume definitions
    Add support for resize-volume.
    Currently, resize-volume (in models.py) calls guest instance's stop_db, via
    _assert_datastore_is_offline. This stop_db definition requires a keyword
    argument do_not_start_on_reboot. This patch corrects stop_db
    implementation, which originally didn't have do_not_start_on_reboot
    keyword argument.
    This patch also implements mount_volume, unmount_volume, and resize_fs.
    It also implements _enable_pgsql_on_boot and _disable_pgsql_on_boot.
    These definitions are required to fix 1410425 (resize-volume bug
    in postgresql).
    The easiest way to verify this bug (and this patch) is to follow
    these steps:
    - trove create ps1 100 --datastore postgresql --datastore_version
      9.3 --size 1 --databases db1 --users user1:password1
    - trove list (note the ID, and verify instance is active)
    - trove resize-volume <id> 2
    - trove list (verify that size is 2)
    
    Closes-Bug: #1410425
    Change-Id: Ifc245dbf27af0269a2b71995bc2c6e2733420a81

--- trove/guestagent/datastore/postgresql/service/process.py
+++ trove/guestagent/datastore/postgresql/service/process.py
@@ -30,6 +30,7 @@ class PgSqlProcess(object):
     """Mixin that manages the PgSql process."""
 
     def start_db(self, context):
+        self._enable_pgsql_on_boot()
         """Start the PgSql service."""
         cmd = operating_system.service_discovery(PGSQL_SERVICE_CANDIDATES)
         LOG.info(
@@ -44,8 +45,30 @@ class PgSqlProcess(object):
             timeout=30
         )
 
-    def stop_db(self, context):
+    def _enable_pgsql_on_boot(self):
+        try:
+            pgsql_service = operating_system.service_discovery(
+                PGSQL_SERVICE_CANDIDATES)
+            utils.execute_with_timeout(pgsql_service['cmd_enable'],
+                                       shell=True)
+        except KeyError:
+            LOG.exception(_("Error enabling PostgreSQL start on boot."))
+            raise RuntimeError("Service is not discovered.")
+
+    def _disable_pgsql_on_boot(self):
+        try:
+            pgsql_service = operating_system.service_discovery(
+                PGSQL_SERVICE_CANDIDATES)
+            utils.execute_with_timeout(pgsql_service['cmd_disable'],
+                                       shell=True)
+        except KeyError:
+            LOG.exception(_("Error disabling PostgreSQL start on boot."))
+            raise RuntimeError("Service is not discovered.")
+
+    def stop_db(self, context, do_not_start_on_reboot=False):
         """Stop the PgSql service."""
+        if do_not_start_on_reboot:
+            self._disable_pgsql_on_boot()
         cmd = operating_system.service_discovery(PGSQL_SERVICE_CANDIDATES)
         LOG.info(
             _("{guest_id}: Stopping database engine with command ({command}).")
