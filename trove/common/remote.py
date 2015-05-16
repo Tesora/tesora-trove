2421a5b8f7d47f40d4212bee22bc8ea2d9c1b351
Doug Shelley <doug@parelastic.com>
2015-05-16 15:26:46 -0400

    Fix authentication error is periodic tasks
    The issue here is that "project_id" is being used to pass in
    a tenant UUID. According to a comment in the novaclient code,
    project_id is a the tenant name. Changing this parameter to
    tenant_id fixes the issue.
    
    Change-Id: I29222b62f0e00c6ae5a182988035188d185039e0
    Closes-bug: dbaas-473

--- trove/common/remote.py
+++ trove/common/remote.py
@@ -98,7 +98,7 @@ def nova_client(context):
                            endpoint_type=CONF.nova_compute_endpoint_type)
 
     client = Client(context.user, context.auth_token,
-                    bypass_url=url, project_id=context.tenant,
+                    bypass_url=url, tenant_id=context.tenant,
                     auth_url=PROXY_AUTH_URL)
     client.client.auth_token = context.auth_token
     client.client.management_url = url
