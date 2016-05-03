772df8c16b0c7d997bcfcc1866a7bc81c2416626
Petr Malik <pmalik@tesora.com>
2016-05-03 11:18:32 -0400

    A bunch of fixes for scenario tests on EE-1.7
     - Skip cluster tests on MariaDB (not supported).
     - Skip cluster root enable on PXC.
     - Workaround around the known API user-filter issue.
       (see https://review.openstack.org/#/c/309190/)
    
    Change-Id: Ic4c3fb3a9e3fcb62e96b44f69c62567df42e89a9

--- trove/tests/int_tests.py
+++ trove/tests/int_tests.py
@@ -204,9 +204,8 @@ register(["mysql_supported", "percona_supported"], common_groups,
          backup_groups, configuration_groups, database_actions_groups,
          replication_groups, root_actions_groups, user_actions_groups)
 register(["mariadb_supported"], common_groups,
-         backup_groups, cluster_actions_groups, configuration_groups,
-         database_actions_groups, replication_groups, root_actions_groups,
-         user_actions_groups)
+         backup_groups, configuration_groups, database_actions_groups,
+         replication_groups, root_actions_groups, user_actions_groups)
 register(["mongodb_supported"], common_groups,
          backup_groups, cluster_actions_groups, configuration_groups,
          database_actions_groups, root_actions_groups, user_actions_groups)
