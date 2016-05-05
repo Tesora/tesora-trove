354c842a20670d5ac7186e5412a178121707f127
Petr Malik <pmalik@tesora.com>
2016-05-05 16:43:51 -0400

    Disable CouchDB backup test
    CouchDB does not support backups in EE-1.7
    
    Change-Id: Ia6165d19b808396d416655ac5bc4ded20489152a

--- trove/tests/int_tests.py
+++ trove/tests/int_tests.py
@@ -196,7 +196,7 @@ register(["cassandra_supported"], common_groups,
          root_actions_groups, cluster_actions_groups)
 register(["couchbase_supported"], common_groups, user_actions_groups,
          root_actions_groups, backup_groups, cluster_actions_groups)
-register(["couchdb_supported"], common_groups, backup_groups)
+register(["couchdb_supported"], common_groups)
 register(["postgresql_supported"], common_groups,
          backup_groups, database_actions_groups, configuration_groups,
          root_actions_groups, user_actions_groups, replication_groups)
