6c5cd501ff0b39d0e9933a2f1d73be353b8d00e4
Petr Malik <pmalik@tesora.com>
2016-06-07 11:54:00 -0400

    Fix Couchbase memory quota computation
    The quota should be at least 256MB but
    no more than 80% of the total available RAM.
    
    Change-Id: If6b7f130e0bafe36c1fb58b30116f627b90c79aa
    Closes-Bug: DBAAS-1972

--- trove/tests/unittests/guestagent/test_couchbase_manager.py
+++ trove/tests/unittests/guestagent/test_couchbase_manager.py
@@ -211,3 +211,12 @@ class GuestAgentCouchbaseManagerTest(trove_testtools.TestCase):
                                       'ramQuota': '268435456'},
                           'bucket2': {'saslPassword': 'password2',
                                       'ramQuota': '134217728'}}, bucket_list)
+
+    def test_ramsize_quota_mb(self):
+        app = couch_service.CouchbaseApp(Mock())
+
+        app.available_ram_mb = 1024
+        self.assertEqual(819, app.ramsize_quota_mb)
+
+        app.available_ram_mb = 128
+        self.assertEqual(256, app.ramsize_quota_mb)
